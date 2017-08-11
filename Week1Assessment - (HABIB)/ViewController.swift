f2f523f3deb4ccde2e1910a138b7f8f4fe71070d
Habib Zahrrin Chang Fard <habib@Habibs-MacBook-Pro.local>
2017-08-11 16:06:12 +0800

    message here

--- Week1Assessment - (HABIB)/ViewController.swift
+++ Week1Assessment - (HABIB)/ViewController.swift
@@ -9,7 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    
+    var num1 : Int = 0
+    var num2 : Int = 0
+    var resultadd : Int = 0
+    var resultMinus : Int = 0
+    var resultMul : Int = 0
+    var resutlDiv : Int = 0
+    
+    @IBOutlet weak var num1Text: UITextField!
+    
+    @IBOutlet weak var num2Text: UITextField!
+    
+    
+    @IBAction func ButtonCalculate(_ sender: Any) {
+        
+        num1 = Int(num1Text.text!)!
+        num2 = Int(num2Text.text!)!
+        
+        resultadd = num1 + num2
 
+        
+    }
+
+    
+    @IBAction func buttonMinus(_ sender: Any) {
+        
+        num1 = Int(num1Text.text!)!
+        num2 = Int(num2Text.text!)!
+        
+        resultadd = num1 - num2
+    }
+    
+    @IBAction func BUttonDivide(_ sender: Any) {
+        num1 = Int(num1Text.text!)!
+        num2 = Int(num2Text.text!)!
+        
+        resultadd = num1 / num2
+        
+    }
+    
+    
+    @IBAction func buttonTimes(_ sender: Any) {
+        num1 = Int(num1Text.text!)!
+        num2 = Int(num2Text.text!)!
+        
+        resultadd = num1 * num2
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
